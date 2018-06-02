570eadab7d5395f0442241f82078b7efa85ea520
saygin <saygindogu@gmail.com>
2018-06-02 18:31:28 +0300

    [Blog] Created the model for blog and made the migrations.

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogPost( models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    publication_date = models.DateTimeField()
