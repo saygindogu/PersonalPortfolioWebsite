61a07b80a872ee73737e6b4591c20f423c78cc4a
saygin <saygindogu@gmail.com>
2018-06-02 17:31:39 +0300

    [Jobs] Job model is added and the migrations are done.

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#from . import settings #For Pycharm to see variables inside the settings.
+from django.conf import settings #Pycharm cannot see the variables in this way
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
