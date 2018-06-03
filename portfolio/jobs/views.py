e8370c953aa073b6888dbf9b9988a08af3c8b8b2
saygin <saygindogu@gmail.com>
2018-06-03 14:29:47 +0300

    [HomePage] Added home page.

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Job
+
 # Create your views here.
+
+def home( request ):
+    steve = Job.objects
+
+    return render(request, 'jobs/home.html', { 'jobs': steve})
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -19,7 +19,9 @@ from django.urls import path
 #from . import settings #For Pycharm to see variables inside the settings.
 from django.conf import settings #Pycharm cannot see the variables in this way
 from django.conf.urls.static import static
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs.views.home, name="home")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
